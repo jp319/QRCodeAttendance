428b3018e2e7b5bb87e9b521a930947a4b6989ce
Blooh123 <bloohgamedev@gmail.com>
2025-05-07 18:06:13 +0800

    d

--- app/views/adminHome.php
+++ app/views/adminHome.php
@@ -175,7 +175,7 @@ $allowed_pages = ['Dashboard', 'Students', 'Attendance', 'Users', 'ProfileAdmin'
                 showLoading();
 
                 // Load content dynamically from loadContent.php
-                fetch(`loadContent.php?page=${page}`)
+                fetch(`?page=${page}`)
                     .then(response => {
                         if (!response.ok) throw new Error("Page not found.");
                         return response.text();
